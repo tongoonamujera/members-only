c785541693424f1a86185003e96d9545e5491eaf
tongoonamujera <tongoonamujera@gmail.com>
2021-08-11 20:25:36 +0200

    Add more configurations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         mount_uploader :image, FileUploader
+         has_many :posts
 end
