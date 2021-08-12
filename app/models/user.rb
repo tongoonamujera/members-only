8c871874736adb397167c65213080bac62f3ca5c
tongoonamujera <tongoonamujera@gmail.com>
2021-08-12 10:56:08 +0200

    Fix errors in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         mount_uploader :image, FileUploader
-         has_many :posts
+        :recoverable, :rememberable, :validatable
+        mount_uploader :image, FileUploader
+        has_many :posts
 end
