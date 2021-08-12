ff65f9c3bd2f11d37b2846d820cdb8a3c2f4aa60
tongoonamujera <tongoonamujera@gmail.com>
2021-08-12 14:52:22 +0200

    Add requested changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: %i[index]
   before_action :set_post, only: %i[show edit update destroy]
 
   # GET /posts or /posts.json
