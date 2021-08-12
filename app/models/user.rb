b7e5bb8c2171c54e7f84d386a073c0f2a8c685a7
tongoonamujera <tongoonamujera@gmail.com>
2021-08-12 13:30:13 +0200

    Add validations to user and post

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   mount_uploader :image, FileUploader
   has_many :posts
+  validates :username, presence: true
 end
